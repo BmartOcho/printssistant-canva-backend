5b50e5d709b14ec449287a024846fe7fd75ee76d
BmartOcho <109639555+BmartOcho@users.noreply.github.com>
2025-10-31 09:13:51 -0500

    Refactor server.js and update import path in index.js

--- api/server.js
+++ api/server.js
@@ -10,6 +10,15 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+  next();
+});
 
 // ---------- Config helpers ----------
 const CANVA_CLIENT_ID = process.env.CANVA_CLIENT_ID;
